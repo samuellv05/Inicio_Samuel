e9554a33d1be7342f6a6e7727a00fca1e2ffb06e
dani <dani@DESKTOP-12A10G8>
2024-04-29 08:53:02 +0200

    modificacion para samuel

--- src/ej/Hola.java
+++ src/ej/Hola.java
@@ -5,6 +5,7 @@ public class Hola {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola soy samuel");
+		System.out.println("hola soy daniel 29/04/2002");
 
 	}
 
